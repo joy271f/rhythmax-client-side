d096590f70a9231ebdf017ed6992d75ae0529a23
joychandradas <joychandradas007@gmail.com>
2023-06-13 18:43:31 +0600

    SignIn and SignUp Form Design Ready

--- src/pages/Login/SignIn.jsx
+++ src/pages/Login/SignIn.jsx
@@ -1,8 +1,60 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
+    // TODO : Reset
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }
     return (
-        <div>
-            
+        <div className="hero">
+            <div className="hero-content mt-8 flex-col lg:flex-row">
+                <div className="text-center lg:text-left md:w-1/2">
+                    <img src="/login.jpg" alt="" />
+                </div>
+                <div className="card flex-shrink-0 md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    placeholder="example@gmail.com"
+                                    {...register("email", { required: true })}
+                                    name="email"
+                                    className="input input-bordered"
+                                />
+                                {errors.email && <span className="text-amber-300">Email field is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="password"
+                                    {...register("password", { required: true })}
+                                    name="password"
+                                    className="input input-bordered"
+                                />
+                                {errors.password?.type === 'required' && <p className="text-amber-300">Password is required</p>}
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <p className="text-pink-600"></p>
+                            <div className="form-control mt-6">
+                                <input className="btn bg-pink-600 text-white" type="submit" value="Sign in" />
+                            </div>
+                            <div className="divider">OR</div>
+                            <span className='mx-auto'>Are You New Here? Please <Link to='/signup' className="text-pink-500">Sign Up</Link></span>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Login/SignUp.jsx
+++ src/pages/Login/SignUp.jsx
@@ -1,8 +1,94 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+    // TODO : Reset
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }
     return (
-        <div>
-            <h1>signup</h1>
+        <div className="hero">
+            <div className="hero-content mt-8 flex-col lg:flex-row">
+                <div className="text-center lg:text-left md:w-1/2">
+                    <img src="/login.jpg" alt="" />
+                </div>
+                <div className="card flex-shrink-0 md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    placeholder="name"
+                                    {...register("name", { required: true })}
+                                    name="name"
+                                    className="input input-bordered"
+                                />
+                                {errors.name && <span className="text-amber-300">Name field is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">PhotoURL</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    placeholder="photoURL"
+                                    {...register("photo", { required: true })}
+                                    name="photo"
+                                    className="input input-bordered"
+                                />
+                                {errors.photo && <span className="text-amber-300">PhotoURL field is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    placeholder="example@gmail.com"
+                                    {...register("email", { required: true })}
+                                    name="email"
+                                    className="input input-bordered"
+                                />
+                                {errors.email && <span className="text-amber-300">Email field is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="password"
+                                    {...register("password", {
+                                        required: true,
+                                        minLength: 6,
+                                        maxLength: 20,
+                                        pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                                    })}
+                                    name="password"
+                                    className="input input-bordered"
+                                />
+                                {errors.password?.type === 'required' && <p className="text-amber-300">Password is required</p>}
+                                {errors.password?.type === 'minLength' && <p className="text-amber-300">Password must be 6 character</p>}
+                                {errors.password?.type === 'maxLength' && <p className="text-amber-300">Password must be less then 20 character</p>}
+                                {errors.password?.type === 'pattern' && <p className="text-amber-300">Password must have one uppercase, one spatial character, one number, one lowercase</p>}
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <p className="text-pink-600"></p>
+                            <div className="form-control mt-6">
+                                <input className="btn bg-pink-600 text-white" type="submit" value="Sign Up" />
+                            </div>
+                            <div className="divider">OR</div>
+                            <span className='mx-auto'>Already User? Please <Link to='/signin' className="text-pink-500">Sign In</Link></span>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
